df95787199e17409281063ca1bca3e57eaf54fe5
Fxvoid <fxvoid803@gmail.com>
2023-11-01 17:30:56 +0200

    option to select region + save to localstorage

--- src/components/Blocks/Header/HomeHeader/HomeHeaderElements/Logo/Location/Location.tsx
+++ src/components/Blocks/Header/HomeHeader/HomeHeaderElements/Logo/Location/Location.tsx
@@ -1,18 +1,50 @@
-import React, { FC } from 'react'
-import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
-import styles from './location.module.css'
+import React, { FC, useEffect, useState } from "react";
+import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
+import styles from "./location.module.css";
 
-interface LocationProps {
-    city: string;
-}
+const cities = ["Москва", "Брянск", "Коломна", "Орехово", "Зуево"];
 
-const Location:FC<LocationProps> = ({city}) => {
+const Location: FC = () => {
+  const [currentCity, setCurrentCity] = useState<string>("Москва");
+  const [showCityList, setShowCityList] = useState<boolean>(false);
+
+  const toggleShowCity = () => {
+    setShowCityList(!showCityList);
+  };
+
+  useEffect(() => {
+    const savedCity = localStorage.getItem("city");
+    if (savedCity) setCurrentCity(savedCity);
+  }, []);
+
+  const handleCitySelected = (city: string) => {
+    setCurrentCity(city);
+    toggleShowCity();
+    localStorage.setItem("city", city);
+  };
+
   return (
-    <div className={styles.location}>
-        <LocationOnOutlinedIcon style={{fontSize: 'small', display: 'block'}}/>
-        <p>{city}</p>
+    <div className={styles.location_wrapper}>
+      <div className={styles.location} onClick={toggleShowCity}>
+        <LocationOnOutlinedIcon
+          style={{ fontSize: "small", display: "block" }}
+        />
+        <p>{currentCity}</p>
+      </div>
+      <div
+        className={styles.location_select}
+        style={showCityList ? { display: "block" } : { display: "none" }}
+      >
+        {cities.map((city) =>
+          city !== currentCity ? (
+            <p key={city} onClick={() => handleCitySelected(city)}>
+              {city}
+            </p>
+          ) : null
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Location
+export default Location;
