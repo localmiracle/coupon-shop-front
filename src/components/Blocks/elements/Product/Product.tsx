365b0628d022b016577076da053946d50500b7d4
Fxvoid <fxvoid803@gmail.com>
2023-11-05 18:24:47 +0200

    product component rework

--- src/components/Blocks/elements/Product/Product.tsx
+++ src/components/Blocks/elements/Product/Product.tsx
@@ -1,49 +1,62 @@
-import React, { FC, useEffect, useState } from 'react'
-import styles from './Product.module.css'
-import Image from 'next/image';
-import ClearIcon from '@mui/icons-material/Clear';
-import EditNoteIcon from '@mui/icons-material/EditNote';
+import React, { FC, useEffect, useState } from "react";
+import styles from "./Product.module.css";
+import Image from "next/image";
+import ClearIcon from "@mui/icons-material/Clear";
+import EditNoteIcon from "@mui/icons-material/EditNote";
 
-interface ProductProps{
-    id: string | number;
-    image: string;
-    name: string;
-    description: string;
-    level: number;
-    price: number;
-    style: any;
-    handleEditProduct?:any, 
-    handleDeleteProduct?:any,
+interface ProductProps {
+  id: string | number;
+  image: string;
+  name: string;
+  description: string;
+  level: number;
+  price: number;
+  style?: any;
 }
 
-const Product:FC<ProductProps> = ({id, image, name, description, level, price, style, handleEditProduct, handleDeleteProduct}) => {
-  const [isAdmin, setIsAdmin] = useState<boolean>(false)
-  useEffect(()=>{
-    if(localStorage.getItem('adminToken')){
-      setIsAdmin(true)
-    } else {
-      setIsAdmin(false)
-    }
-  },[])
+const Product: FC<ProductProps> = ({
+  image,
+  name,
+  description,
+  level,
+  price,
+  style,
+}) => {
+  let levelString = "";
+
+  switch (level) {
+    case 1:
+      levelString = "Минимум";
+      break;
+    case 2:
+      levelString = "Стандарт";
+      break;
+    case 3:
+      levelString = "Премиум";
+      break;
+    default:
+      levelString = "Минимум";
+      break;
+  }
+
   return (
-    <div className={styles.list__item} style={style}>
-        {isAdmin ?<div className={styles.edits}>
-          <ClearIcon style={{color: 'red',cursor:'pointer'}} onClick={handleDeleteProduct}/>
-          <EditNoteIcon style={{cursor:'pointer'}} onClick={handleEditProduct}/>
-        </div> : null}
-        <Image src={image} alt={''} width={209} height={233}/>
-        <div className={styles.info}>
-            <div>
-                <h3>{name}</h3>
-                <p className={styles.description}>{description}</p>
-            </div>
-            <p className={styles.level}>Уровень: {level}</p>
-            <p className={styles.price}>{price} ₽</p>
-            
-            <button className={styles.btn}>Купить</button>
+    <div className={styles.card} style={style}>
+      <div className={styles.card_image}>
+        <p>IMAGE</p>
+      </div>
+      <div className={styles.card_info}>
+        <div>
+          <h2>{name}</h2>
+          <p className={styles.description}>{description}</p>
         </div>
+        <p className={styles.level}>Уровень: {levelString}</p>
+        <div className={styles.price}>
+          <span>{price} ₽</span>
+        </div>
+        <button>КУПИТЬ</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
